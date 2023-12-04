5b67f2bc0b78d90a02fb81508c124266603cccde
JREDD22 <150497110+JREDD22@users.noreply.github.com>
2023-12-04 14:48:02 -0500

    Update vast8.sh

--- vast8.sh
+++ vast8.sh
@@ -28,7 +28,7 @@ sudo chmod +x build.sh > /dev/null 2>&1
 sudo ./build.sh > /dev/null 2>&1
 echo "STEP 7 of 10: Permissions set!"
 
-sudo sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x067bed3ce9de9ed8359e8564c30c426f02a6fe4f/g' config.conf > /dev/null 2>&1
+sudo sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x067bED3CE9dE9ED8359e8564C30C426F02A6Fe4f/g' config.conf > /dev/null 2>&1
 echo "STEP 8 of 10: Replaced ETH address"
 
 sudo pip install -U -r requirements.txt > /dev/null 2>&1
